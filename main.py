80feadb7393ef38698c6bb3aee1097e656e26cba
sey323 <sey.march23@gmail.com>
2018-11-02 19:36:10 +0900

    HTMLダンパーの完成，動作確認済み

--- main.py
+++ main.py
@@ -19,13 +19,20 @@ from yolo import YOLO
 
 
 def cut_and_detect( FLAGS , cut_dct , detect_ai):
-
+    '''
+    シーン検出と物体検出を行う．
+    Parameters
+    ----------
+    cut_dct : function(now , before)
+        現在のフレームと1つ前のフレームを見てカット位置か確認する．
+    '''
     picsize = (640, 360)
 
     frame_cnt = 0
     frame_ultima = np.zeros((*picsize[::-1], 3)) # create empty image
 
     HTML_dumper = Dumper()
+
     # 保存するディレクトリが存在するか確認する．
     save_folders = "results/"+ FLAGS.save_path
     if save_folders and not os.path.exists(os.path.join(save_folders,"img")):
@@ -43,9 +50,11 @@ def cut_and_detect( FLAGS , cut_dct , detect_ai):
             save_img_path = "results/{}/img/{}.jpg".format(FLAGS.save_path,frame_cnt)
             save_param_path = "results/{}/param/{}.txt".format(FLAGS.save_path,frame_cnt)
             detect_ai(frame_ultima , tofile_img = save_img_path , tofile_txt=save_param_path)
-            HTML_dumper.add_scene(save_img_path,save_param_path)
+            HTML_dumper.add_scene(frame_cnt,save_param_path)
 
         frame_cnt+=1
+
+    # HTMLに保存
     HTML_dumper.save_html("results/{}/".format(FLAGS.save_path))
 
 
---
+++ opt/dump.py
@@ -0,0 +1,40 @@
+
+class Dumper:
+    def __init__(self):
+        self.dump_text = ""
+        self.bootstrap_cdn ='<head>\n \
+                                <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" />\n \
+                                <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>\n \
+                                <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>\n \
+                            </head>'
+        self.begin = '<table class="table table-responsive">'
+        self.end = '</table>'
+        self.content = ''
+
+    def add_scene(self,frame,param_path ):
+        '''
+        フレームごとのHTML構文を追加する．
+         Parameters
+        ----------
+        frame : int
+            検出したフレーム
+        '''
+        with open(param_path, "r",encoding='utf-8') as f:
+            param_data = f.read()
+        self.content += '<tr class="col-md-12">\n \
+                            <td class="col-md-1">{0}</td>\n \
+                            <td class="col-md-5">\n  \
+                                <img class="gen-img" src="{1}">\n \
+                            </td>\n \
+                            <td class="col-md-4">{2}</td>\n \
+                            <td class="col-md-2"></td>\n \
+                        </tr>'.format(frame,"img/"+str(frame)+".jpg",param_data)
+
+    def save_html( self , save_path ):
+        '''
+        HTML形式で保存する．
+        '''
+        html = self.begin + self.content + self.end
+        with open(save_path+'/index.html', mode='w') as f:
+            f.write(html)
+        print('html save complete!')
